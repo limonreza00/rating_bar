11575fc03538a9174c615a36aed3479264a1fadd
Limon <limonreza3.1416@gmail.com>
2024-03-09 23:20:28 +0600

    All logic implement 1. set text in Adjective according to Rating 2. set rating value in float and show on toash with buttonOnClick

--- app/src/main/java/com/torchtech/ratingbar/MainActivity.kt
+++ app/src/main/java/com/torchtech/ratingbar/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.RatingBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -22,5 +23,21 @@ class MainActivity : AppCompatActivity() {
         val ratingBar = findViewById<RatingBar>(R.id.ratingBar)
         val display = findViewById<TextView>(R.id.display)
         val rateButton = findViewById<Button>(R.id.rateButton)
+
+        display.text= getString(R.string.good)
+
+        ratingBar.setOnRatingBarChangeListener { _, rating, _ ->
+            when (rating.toInt()){
+                1 -> display.text = getString(R.string.very_bad)
+                2 -> display.text = getString(R.string.bad)
+                3 -> display.text = getString(R.string.good)
+                4 -> display.text = getString(R.string.very_good)
+                5 -> display.text = getString(R.string.excellent)
+                else -> display.text = getString(R.string.no_rating)
+            }
+            rateButton.setOnClickListener {
+                Toast.makeText(this,"Your rating is $rating",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
