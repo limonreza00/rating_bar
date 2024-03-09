7ab81ece6c89c90696f05624013f26140ef5678e
Limon <limonreza3.1416@gmail.com>
2024-03-09 22:53:08 +0600

    simple rating bar create

---
+++ app/src/main/java/com/torchtech/ratingbar/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.torchtech.ratingbar
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val ratingBar = findViewById<RatingBar>(R.id.ratingBar)
+        val display = findViewById<TextView>(R.id.display)
+        val rateButton = findViewById<Button>(R.id.rateButton)
+    }
+}
